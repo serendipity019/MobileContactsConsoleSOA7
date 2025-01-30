91355550e837a3d987a848c95899e788a7d5db71
serendipity019 <navagio09@gmail.com>
2025-01-30 15:53:06 +0200

    update

--- src/gr/aueb/cf/mobile_contacts/exceptions/PhoneNumberAlreadyExistsException.java
+++ src/gr/aueb/cf/mobile_contacts/exceptions/PhoneNumberAlreadyExistsException.java
@@ -1,6 +1,10 @@
 package gr.aueb.cf.mobile_contacts.exceptions;
 
+import java.io.Serial;
+
 public class PhoneNumberAlreadyExistsException extends Exception {
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     public PhoneNumberAlreadyExistsException(String message) {
         super(message);
